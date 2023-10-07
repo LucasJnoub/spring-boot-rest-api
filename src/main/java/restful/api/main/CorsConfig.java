016744d50d183610654d230fe88eff3d555fed00
LucasJnoub <lucas.jnoub@gmail.com>
2023-10-07 15:00:18 -0300

    pushing

--- src/main/java/restful/api/main/CorsConfig.java
+++ src/main/java/restful/api/main/CorsConfig.java
@@ -3,23 +3,27 @@ package restful.api.main;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 public class CorsConfig {
-
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("*"); // Permitir todas as origens (liberar geral)
-        corsConfig.addAllowedMethod("*"); // Permitir todos os métodos HTTP
-        corsConfig.addAllowedHeader("*"); // Permitir todos os cabeçalhos
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("DELETE");
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
         source.registerCorsConfiguration("/**", corsConfig);
 
-        return new CorsWebFilter((CorsConfigurationSource) source);
+        return new CorsWebFilter(source);
     }
 }
