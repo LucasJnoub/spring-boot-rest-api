856ea30c0e7f71e5c251882b5af61b5ccb6cfc8d
LucasJnoub <lucas.jnoub@gmail.com>
2023-10-07 15:31:42 -0300

    adding cors policy

--- src/main/java/restful/api/main/CorsConfig.java
+++ src/main/java/restful/api/main/CorsConfig.java
@@ -2,15 +2,18 @@ package restful.api.main;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
-import java.util.Collections;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfig {
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    public class CorsConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("*");
+        }
     }
-}
 
